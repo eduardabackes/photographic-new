db90eddc15ebc5417e1aae3362f773e9f1e6a062
eduardabackes <dudabackesk@gmail.com>
2025-03-22 19:10:34 +0000

    Image adjustments

--- js/twilio.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const twilio = require("twilio");
-
-// Pegando credenciais do .env
-const accountSid = '';
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new twilio(accountSid, authToken);
-
-module.exports = client;
